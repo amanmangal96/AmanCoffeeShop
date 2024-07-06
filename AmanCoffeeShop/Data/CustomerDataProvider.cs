bd0b41e0d847d9f692f281ea2dde98c13d597b82
aman <amanmangal2596@gmail.com>
2024-07-06 14:56:55 +0530

    Add customerViewModel

---
+++ AmanCoffeeShop/Data/CustomerDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using AmanCoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmanCoffeeShop.Data
+{
+    public interface ICustomerDataProvider
+    {
+        Task<IEnumerable<Customer>> GetAllAsync();
+    }
+    internal class CustomerDataProvider : ICustomerDataProvider
+    {
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            await Task.Delay(100);
+            return new List<Customer> 
+            { 
+              new Customer { Id=1 , FirstName="Aman", LastName="mangal", isDeveloper=true},
+              new Customer { Id=2 , FirstName="Ayush", LastName="mangal", isDeveloper=true},
+              new Customer { Id=3 , FirstName="Arun", LastName="Singh", isDeveloper=false},
+              new Customer { Id=4 , FirstName="Ram", LastName="Bansal", isDeveloper=false},
+              new Customer { Id=5 , FirstName="Rahul", LastName="Sharma", isDeveloper=false},
+              new Customer { Id=6 , FirstName="Vivek", LastName="Tyagi", isDeveloper=false},
+            };
+        }
+    }
+}
---
+++ AmanCoffeeShop/ViewModel/CustomersViewModel.cs
@@ -0,0 +1,34 @@
+﻿using AmanCoffeeShop.Data;
+using AmanCoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmanCoffeeShop.ViewModel
+{
+    public class CustomersViewModel
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+        }
+        public ObservableCollection<Customer> Customers { get; } = new ObservableCollection<Customer>();
+
+        public async Task LoadAllAsync()
+        {
+            if(Customers != null)
+            {
+                return;
+            }
+            var customer = await _customerDataProvider.GetAllAsync();
+            foreach(var item in customer)
+            {
+                Customers.Add(item);
+            }
+        }
+    }
+}
